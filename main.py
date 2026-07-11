d1b68834fb90f407d99e5ea7f926d94d87874fe4
nero3k <souhayltiali63@gmail.com>
2026-07-12 01:59:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,28 +2,14 @@ import yt_dlp
 import os
 
 def process_video(url):
-    # 1. التحميل
     ydl_opts = {
         'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]',
         'outtmpl': 'input.mp4',
+        'cookiefile': 'cookies.txt', # دابا غيلقى الملف واجد فالبلاصة
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
     
-    # 2. التقطاع (كيقطع 30 ثانية الأولى)
     os.system("ffmpeg -i input.mp4 -ss 00:00:10 -t 30 -c copy output_short.mp4")
 
-# حط هنا الرابط ديال الفيديو اللي بغيتي تقطع
 process_video("https://www.youtube.com/watch?v=Rb5L_kagUZ4")
-ydl_opts = {
-        'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]',
-        'outtmpl': 'input.mp4',
-        'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
-    }
-import yt_dlp
-import os
-
-# هاد الكود كياخد الكوكيز من البلاصة السرية
-cookies = os.getenv("COOKIES")
-with open("cookies.txt", "w") as f:
-    f.write(cookies)
