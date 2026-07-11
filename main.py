0d72a4bd3ca331dc408849c02c7b44202ff438ac
nero3k <souhayltiali63@gmail.com>
2026-07-12 00:26:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import yt_dlp
 def download_video(url):
     ydl_opts = {
         'format': 'best',
-        'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-        'outtmpl': 'video.mp4',
+        'quiet': False,
+        'no_warnings': False,
+        'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
